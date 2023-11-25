6b44e9b8d7144e78951b9fc64fee8c9f660b38da
senalbulumulle <senal.bulumulle@gmail.com>
2023-11-24 21:31:00 -0500

    Added file: [create-package-use.sh]

---
+++ create-package-use.sh
@@ -0,0 +1,24 @@
+## create-package-use.sh
+#
+# This script allows the ability to create the package.use 
+# components in gentoo.
+
+
+## This is where you have the ability to create a nice greeter 
+## before proceeding to go to the next step. 
+#
+function greeter() {
+    echo """
+---------------------------------------------------------------------
+                    Prepare the package.use....
+---------------------------------------------------------------------
+    """    
+    sleep 3
+}
+
+
+
+
+
+
+greeter
